63e2530d1be9b042eb9c07fdd96ab755bcfa88a8
Dodie <dodiematts@gmail.com>
2019-09-29 16:08:43 -0400

    Initial Commit

---
+++ TipCalc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipCalc
+//
+//  Created by Dodie Matthews on 9/29/19.
+//  Copyright © 2019 Dodie Matthews. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
